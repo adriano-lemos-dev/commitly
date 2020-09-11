53dfdea77015a9763e5fab1fa3396d5e4a90155d
Adriano Lemos <adriano.lemos.dev@gmail.com>
2020-09-11 14:08:42 -0300

    fix: trata erro de commit
    commitly

--- src/commit.js
+++ src/commit.js
@@ -4,13 +4,13 @@ import util from 'util';
 const execp = util.promisify(exec);
 
 export async function commit(tipo = 'feat', mensagem, detalhe) {
-  const { error, stdout } = await execp(
+  const { error } = await execp(
     `git commit -a -m "${tipo}: ${mensagem}" -m " " -m "${
       detalhe ? detalhe : 'commitly'
     }"`
   );
   if (error) {
-    console.error(`exec error: ${error}`);
-    return;
+    console.error(`[commitly] : Commit falhou! 😞`);
+    process.exit(1);
   }
 }
